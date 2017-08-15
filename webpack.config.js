f17b6f9f2c257de0f0bfa9b3e7f5c0fd9c5f8ddf
Han Koh <rhgksrua@gmail.com>
2017-08-15 13:37:59 -0500

    configured webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: './src/inject/inject.js',
+  output: {
+    filename: './build/inject.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+            plugins: [require('babel-plugin-transform-object-rest-spread')]
+          }
+        }
+      }
+    ]
+  }
+}
